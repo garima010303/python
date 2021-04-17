f424545a1a7d70147f76310a79b5723f2d6e08b2
Garima <76618872+garima010303@users.noreply.github.com>
2021-04-18 04:38:46 +0530

    Create guess_the_number.py

---
+++ games/guess_the_number.py
@@ -0,0 +1,17 @@
+import random
+rn = random.randint(1,100)
+print(rn)
+user_number = None
+guesses = 0
+
+while(user_number != rn):
+    user_number = int(input("Guess the number: "))
+    if(user_number==rn):
+        print("Congratulations! You guessed it right!")
+    elif(user_number < rn):
+        print("Bad luck! please try a higher number")
+    else:
+        print("Bad luck! please try a lower number")
+    guesses+= 1
+
+print(f"You guessed the correct number in {guesses} guesses")
