4294ab6bd2d41a00aa370df5b7fdd3cfe41d6c84
Garima <76618872+garima010303@users.noreply.github.com>
2021-04-18 08:27:11 +0530

    updated

--- games/shopping.py
+++ games/shopping.py
@@ -2,6 +2,8 @@ import random
 
 print("Are you a computer or Human being?")
 a = input("Press C for Computer and press H for human being: ")
+products = " "
+
 if(a == "C"):
     print("Choose items that you want to buy: ")  # by random module
 
@@ -13,24 +15,27 @@ if(a == "C"):
         product = random.choice(item_list)
         item_list = ["DairyMilk", "Kitkat", "Butterscotch", "Chocolate",
                      "SilkOreo", "Kurkure", "Unclechips", "Brownies", "done"]
-        if(item_list == 'done'):
-            print("Thank you! Have a good day.")
 
-
-else:
+elif(a == "H"):
     name = input("Enter your name: ")
     print(f"Welcome, {name}.")
     product = input("What do you want to buy?")
+    products += product + " "
     item_list = ["DairyMilk", "Kitkat", "Butterscotch",
                  "Chocolate", "SilkOreo", "Kurkure", "Unclechips", "Brownies"]
-    done = "done"
-    while(product != done):
+    done = " "
+    while(product != "done"):
 
         if product in item_list:
             print(f"{product} is added to list.")
-            product = input("What do you want to buy?")
+            product = input("What do you want to buy next?")
+            products += product + " "
+            print(f"Your final products that you want to buy are: {products}")
         else:
             print("Sorry! The item is not available")
-            product = input("Any other item that you want to buy? ")
-        print(product)
-print("Thank you! Have a good day.")
+            anything = input("Any other item that you want to buy? ")
+
+else:
+    print: "Invalid user, please enter C for Computer and H for human being: "
+
+print("Thank you! Have a good day.\nHope to see you next time!")
